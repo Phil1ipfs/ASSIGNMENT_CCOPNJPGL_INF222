ba8d6a0d68020af365b6d861ee0be66c066457eb
PHP <127711945+Phil1ipfs@users.noreply.github.com>
2023-05-25 16:06:11 +0800

    Add files via upload

---
+++ Exercise5/model/holder.java
@@ -0,0 +1,22 @@
+package model;
+class holderInfo {
+
+    private String name;
+    private String promt;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPromt() {
+        return this.getPromt();
+    }
+
+    public void setPromt(String promt) {
+        this.promt = promt;
+    }
+}
